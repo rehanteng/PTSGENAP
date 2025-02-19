f1041b71ff93e9471c33c8be25d7e4ff39a16b59
rehanteng <rehannajib89@gmail.com>
2025-02-19 14:28:44 +0700

    Update PTS-5.py

---
+++ PTS-5.py
@@ -0,0 +1,5 @@
+def perpangkatan(bilangan, pangkat): # Mendefinisikan fungsi perpangkatan dengan dua parameter
+    return bilangan ** pangkat # Mengembalikan hasil bilangan
+
+print(perpangkatan(2, 3)) # Memanggil fungsi 
+print(perpangkatan(5, 2)) # Memanggil fungsi 
