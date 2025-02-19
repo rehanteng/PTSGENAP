d2b22c140bc87e18ca2cbf0530c240f36a5d232c
rehanteng <rehannajib89@gmail.com>
2025-02-19 14:29:16 +0700

    Update PTS-4.py

---
+++ PTS-4.py
@@ -0,0 +1,6 @@
+def cek_ganjil_genap(n): # Mendefinisikan fungsi
+    return "Ganjil" if n % 2 != 0 else "Genap" #Mengecek apakah n bilangan ganjil atau genap menggunakan operator modulus (%).
+ 
+print(cek_ganjil_genap(7)) # Memanggil fungsi
+print(cek_ganjil_genap(10)) # Memanggil fungsi 
+
